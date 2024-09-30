d7d955e1f00763f4a2faf829c3107450d57330a6
Jaldhi Bhatt <jaldhibhatt@Jaldhis-Laptop.local>
2024-09-29 20:26:34 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import * as Accordion from "$lib/components/ui/accordion";
+    import { cn } from "$lib/utils";
+  </script>
+  
+  <div class={cn("container mx-auto px-4")}>
+    <Accordion.Root>
+      <Accordion.Item value="item-1">
+        <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+        <Accordion.Content>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>
+  </div>
